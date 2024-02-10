6069ee4043f1f60db215f52a1371a6b314e8c8bf
Berkay Kaya <80210343+imberkaykaya@users.noreply.github.com>
2024-02-10 11:39:56 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Workaround
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Degiskenler();
+
+			Console.ReadLine();
+
+
+		}
+
+		private static void Degiskenler()
+		{
+			string mesaj = "Merhaba";
+			double tutar = 100000; // db'den gelir
+			int sayi = 100;
+			bool girisYapmisMi = false;
+
+			string ad = "Berkay";
+			string soyad = "Kaya";
+			int dogumYili = 2000;
+			long TcNo = 1234567890;
+
+			Console.WriteLine(tutar * 0.18);
+			Console.WriteLine(tutar * 1.18);
+		}
+	}
+
+	public class Vatandas()
+	{
+		string ad = "Berkay";
+		string soyad = "Kaya";
+		int dogumYili = 2000;
+		long TcNo = 1234567890;
+	}
+}
